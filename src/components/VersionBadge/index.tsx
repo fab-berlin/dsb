9c7ad46eb84b37ddf625547901a4800e9ad6bd50
Fabian Dahms <dahms@kieler-dahms.de>
2025-04-13 23:07:40 +0200

    extended tailwind config, explicit use of config file

--- src/components/VersionBadge/index.tsx
+++ src/components/VersionBadge/index.tsx
@@ -1,7 +1,7 @@
 import packageJson from '../../../package.json';
 
 const VersionBadge = () => {
-  return <span className={'block text-xs'}>{packageJson.version}</span>;
+  return <span className={'text-xxs block'}>{packageJson.version}</span>;
 };
 
 export default VersionBadge;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      fontSize: {
+        xxs: '0.5rem',
+      },
     },
   },
   plugins: [],
