2073e7e9fface906d0f113461dee92c2a1a3eb88
Fabian Dahms <dahms@kieler-dahms.de>
2025-05-19 12:28:49 +0200

    added weekday to meal overview

--- src/components/MealDetail/index.tsx
+++ src/components/MealDetail/index.tsx
@@ -1,17 +1,27 @@
 import { Meal } from '@/app/store/useMeals';
 import { useEffect, useState } from 'react';
+import { Avatar } from '@radix-ui/themes';
 
 const MealDetail = ({ mealName, mealDate }: Meal) => {
   const [parsedDate, setParsedDate] = useState('');
+  const [weekday, setWeekday] = useState('');
   useEffect(() => {
     const currDate = new Date(mealDate);
+    setWeekday(currDate.toLocaleDateString('de-DE', { weekday: 'short' }));
     setParsedDate(currDate.toLocaleDateString());
   }, [mealDate]);
 
   return (
-    <div className={'rounded-md border bg-gray-800 p-4'}>
-      <span className={'mb-4 block font-bold'}>{parsedDate}</span>
-      <span>{mealName}</span>
+    <div className={'flex flex-row gap-x-6 rounded-md border bg-gray-800 p-4'}>
+      <Avatar
+        fallback={weekday}
+        variant="solid"
+        color="orange"
+      />
+      <div>
+        <span className={'mb-2 block font-bold'}>{parsedDate}</span>
+        <span>{mealName}</span>
+      </div>
     </div>
   );
 };
