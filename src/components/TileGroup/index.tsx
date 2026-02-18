37d4913f64f32f4b29cd289c30ade55850e3a89a
Fabian Dahms <dahms@kieler-dahms.de>
2026-02-18 08:52:24 +0100

    added native select for mobile breakpoint

--- src/components/TileGroup/index.tsx
+++ src/components/TileGroup/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Select } from '@radix-ui/themes';
 import ReplacementTile from '@/components/ReplacementTile';
 import NoResultTile from '@/components/NoResultTile';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
 
 const TileGroup = () => {
   const { replacements } = useClassReplacementStore();
@@ -28,6 +29,15 @@ const TileGroup = () => {
   const handleClassSelect = (e: string) => {
     setChosenClass(e);
   };
+  const handleDateSelectNative = (e: React.FormEvent<HTMLSelectElement>) => {
+    setManualTrigger(true);
+    setChosenDate(e.currentTarget.value);
+    setChosenClass('---');
+  };
+
+  const handleClassSelectNative = (e: React.FormEvent<HTMLSelectElement>) => {
+    setChosenClass(e.currentTarget.value);
+  };
 
   useEffect(() => {
     const uniqueClassesSet = new Set(
@@ -48,65 +58,107 @@ const TileGroup = () => {
     <>
       {Object.keys(replacements).length > 0 && (
         <div className={'flex flex-row flex-wrap justify-between md:justify-start md:gap-x-4'}>
-          <Select.Root
-            onValueChange={handleSelect}
-            size="3"
-            {...(isDateAvailable && !manualTrigger && { value: currentDateString })}
-            {...(isDateAvailable && manualTrigger && { value: chosenDate })}
-          >
-            <Select.Trigger
-              placeholder="wähle den Tag"
-              className="select-trigger-large"
+          <div className={'relative block md:hidden'}>
+            <select
+              className={
+                'w-full appearance-none rounded-md border border-gray-600 py-2 ps-4 pe-8 text-lg focus-visible:outline-none'
+              }
+              onChange={handleDateSelectNative}
             >
-              {chosenDate}
-            </Select.Trigger>
-            <Select.Content
-              position="popper"
-              sideOffset={5}
+              {Object.keys(replacements).map((day) => (
+                <option
+                  key={day}
+                  value={replacements[day].replacementDate.dateString}
+                >
+                  {replacements[day].replacementDate.dateString}
+                </option>
+              ))}
+            </select>
+            <ChevronDownIcon className="text-muted-foreground pointer-events-none absolute top-1/2 right-2 size-4 -translate-y-1/2" />
+          </div>
+
+          <div className={'hidden md:block'}>
+            <Select.Root
+              onValueChange={handleSelect}
+              size="3"
+              {...(isDateAvailable && !manualTrigger && { value: currentDateString })}
+              {...(isDateAvailable && manualTrigger && { value: chosenDate })}
             >
-              <Select.Group>
-                {Object.keys(replacements).map((day) => (
-                  <Select.Item
-                    key={day}
-                    value={replacements[day].replacementDate.dateString}
-                  >
-                    {replacements[day].replacementDate.dateString}
-                  </Select.Item>
-                ))}
-              </Select.Group>
-            </Select.Content>
-          </Select.Root>
+              <Select.Trigger
+                placeholder="wähle den Tag"
+                className="select-trigger-large"
+              >
+                {chosenDate}
+              </Select.Trigger>
+              <Select.Content
+                position="popper"
+                sideOffset={5}
+              >
+                <Select.Group>
+                  {Object.keys(replacements).map((day) => (
+                    <Select.Item
+                      key={day}
+                      value={replacements[day].replacementDate.dateString}
+                    >
+                      {replacements[day].replacementDate.dateString}
+                    </Select.Item>
+                  ))}
+                </Select.Group>
+              </Select.Content>
+            </Select.Root>
+          </div>
 
           {chosenDate && (
-            <div className={'max-w-1/2'}>
-              <Select.Root
-                onValueChange={handleClassSelect}
-                size="3"
-              >
-                <Select.Trigger
-                  placeholder="wähle die Klasse"
-                  className="select-trigger-large"
+            <>
+              <div className={'relative block max-w-1/4 md:hidden'}>
+                <select
+                  className={
+                    'w-full appearance-none rounded-md border border-gray-600 px-4 py-2 text-lg focus-visible:outline-none'
+                  }
+                  onChange={handleClassSelectNative}
                 >
-                  {chosenClass}
-                </Select.Trigger>
-                <Select.Content
-                  position="popper"
-                  sideOffset={5}
+                  <option value="---">wähle die Klasse</option>
+                  {availableClasses.map((name) => (
+                    <option
+                      key={name}
+                      value={name}
+                    >
+                      {name}
+                    </option>
+                  ))}
+                </select>
+                <ChevronDownIcon className="text-muted-foreground pointer-events-none absolute top-1/2 right-2 size-4 -translate-y-1/2" />
+              </div>
+              <div className={'hidden max-w-1/2 md:block'}>
+                <Select.Root
+                  onValueChange={handleClassSelect}
+                  size="3"
                 >
-                  <Select.Group>
-                    <Select.Item value="---">---</Select.Item>
-                    {availableClasses.map((name) => (
-                      <Select.Item
-                        key={name}
-                        value={name}
-                      >
-                        {name}
-                      </Select.Item>
-                    ))}
-                  </Select.Group>
-                </Select.Content>
-              </Select.Root>
-            </div>
+                  <Select.Trigger
+                    placeholder="wähle die Klasse"
+                    className="select-trigger-large"
+                  >
+                    {chosenClass}
+                  </Select.Trigger>
+                  <Select.Content
+                    position="popper"
+                    sideOffset={5}
+                  >
+                    <Select.Group>
+                      <Select.Item value="---">---</Select.Item>
+                      {availableClasses.map((name) => (
+                        <Select.Item
+                          key={name}
+                          value={name}
+                        >
+                          {name}
+                        </Select.Item>
+                      ))}
+                    </Select.Group>
+                  </Select.Content>
+                </Select.Root>
+              </div>
+            </>
           )}
         </div>
       )}
